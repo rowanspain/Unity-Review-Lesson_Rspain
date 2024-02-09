6a29c7c3d3110e99965628156b4a1b6ffdb161ca
rowanspain <125788793+rowanspain@users.noreply.github.com>
2024-02-09 14:14:15 -0800

    UnityReviewLesson_PUSH

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum VegetableType
+    {
+        BEET,
+        CARROT,
+        RADISH
+    }
+
+    public VegetableType typeOfVeggie;
+}

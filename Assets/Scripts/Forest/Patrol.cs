0e98fd4163d4c3ab9673a405167806ed17be2a22
rowanspain <125788793+rowanspain@users.noreply.github.com>
2024-03-06 13:16:03 -0800

    Week 6

---
+++ Assets/Scripts/Forest/Patrol.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    public List<GameObject> waypoints;
+    private NavMeshAgent agent;
+    private const float WP_THRESHOLD = 5.0f;
+    private GameObject currentWP;
+    private int currentWPIndex = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        currentWP = GetNextWaypoint();
+    }
+
+    GameObject GetNextWaypoint()
+    {
+        currentWPIndex++;
+        if (currentWPIndex == waypoints.Count)
+        {
+            currentWPIndex = 0;
+        }
+        return waypoints[currentWPIndex];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, currentWP.transform.position) < WP_THRESHOLD)
+        {
+            currentWP = GetNextWaypoint();
+            agent.SetDestination(currentWP.transform.position);
+        }
+    }
+}
